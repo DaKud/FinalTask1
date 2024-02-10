a49783237f39f6aff85628b6fdfd9341dc6bf6ea
Da <106489968+DaKud@users.noreply.github.com>
2024-02-10 12:49:47 +0300

    Update SearchByID.java

--- FinalTask1/Task1/src/main/java/org/project/View/Commands/SearchByID.java
+++ FinalTask1/Task1/src/main/java/org/project/View/Commands/SearchByID.java
@@ -5,12 +5,12 @@ import org.project.View.ConsoleUI;
 public class SearchByID extends Commands {
 
     public SearchByID(ConsoleUI consoleUI) {
-        super("Поиск по инвентарному номеру", consoleUI);
+        super("Search by ID", consoleUI);
     }
 
     @Override
     public void execute() {
-        System.out.println("ищу...");
+        System.out.println("Searching by ID");
         super.getConsoleUI().searchByID();
     }
 }
