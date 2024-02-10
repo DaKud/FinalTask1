7b1f9b6b9c373d2488410005322b7eaa0f30188a
Da <106489968+DaKud@users.noreply.github.com>
2024-02-10 12:48:03 +0300

    Update ExtractRepo.java

--- FinalTask1/Task1/src/main/java/org/project/View/Commands/ExtractRepo.java
+++ FinalTask1/Task1/src/main/java/org/project/View/Commands/ExtractRepo.java
@@ -5,12 +5,12 @@ import org.project.View.ConsoleUI;
 public class ExtractRepo extends Commands {
 
     public ExtractRepo(ConsoleUI consoleUI) {
-        super("Достать из архива...", consoleUI);
+        super("Extract outta repository", consoleUI);
     }
 
     @Override
     public void execute() {
-        System.out.println("достаю...");
+        System.out.println("Extracting");
         super.getConsoleUI().extractRepo();
     }
 }
