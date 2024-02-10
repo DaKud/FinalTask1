267f2996586a1930172bcb3ff8c535f81a33524f
Da <106489968+DaKud@users.noreply.github.com>
2024-02-10 12:54:37 +0300

    Update Main.java

--- FinalTask1/Task1/src/main/java/org/project/Main.java
+++ FinalTask1/Task1/src/main/java/org/project/Main.java
@@ -2,9 +2,7 @@ package org.project;
 
 import org.project.View.ConsoleUI;
 
-/**
- * запуск программы
- */
+
 public class Main {
     public static void main(String[] args) {
         ConsoleUI consoleUI = new ConsoleUI();
